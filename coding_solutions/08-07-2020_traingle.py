1f2f7d7950c7b2bfc8904b5a1333e4b616817bef
prasannakulal16 <prasannapatla16@gmail.com>
2020-07-08 20:23:09 +0530

    08/07/2020

---
+++ coding_solutions/08-07-2020_traingle.py
@@ -0,0 +1,6 @@
+a=int(input("Enter first side: "))
+b=int(input("Enter second side: "))
+c=int(input("Enter third side: "))
+s=(a+b+c)/2
+area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+print("Area of the triangle is: ",round(area,2))
